aa430579d5e0813877ded36287549db25bfb02a8
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-03-02 00:11:37 -0500

    Removed old MVC code and renamed project to RazorPagesModule

---
+++ src/RazorPagesModule/Pages/IndexModel.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using DotNetNuke.Web.Mvc.RazorPages.SDK;
+
+namespace DNNSummit.MVCModule1.Pages
+{
+    public class IndexModel : DnnPageModel
+    {
+        public IndexModel()
+        {            
+        }
+
+        // TODO - Add on Get and on post handlers, see - https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?tabs=visual-studio
+
+        public string WelcomeMessage => "Hello DNN Razor Pages, it's a brand new world!";
+    }
+}
